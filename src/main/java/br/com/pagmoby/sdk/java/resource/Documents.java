2f1a3f510a8aa70002abab28db5cf7ba3a0b06e2
william-fernandes <william-fernandes@basis.com.br>
2018-08-29 17:34:23 -0300

    Model Documents

---
+++ src/main/java/br/com/pagmoby/sdk/java/resource/Documents.java
@@ -0,0 +1,81 @@
+
+package br.com.pagmoby.sdk.java.resource;
+
+import java.util.List;
+
+public class Documents {
+
+    private String resource;
+    private String uri;
+    private List<Item> items = null;
+    private Integer limit;
+    private Integer offset;
+    private Boolean hasMore;
+    private Integer queryCount;
+    private Integer total;
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public Boolean getHasMore() {
+        return hasMore;
+    }
+
+    public void setHasMore(Boolean hasMore) {
+        this.hasMore = hasMore;
+    }
+
+    public Integer getQueryCount() {
+        return queryCount;
+    }
+
+    public void setQueryCount(Integer queryCount) {
+        this.queryCount = queryCount;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+}
---
+++ src/main/java/br/com/pagmoby/sdk/java/resource/Owner.java
@@ -0,0 +1,35 @@
+
+package br.com.pagmoby.sdk.java.resource;
+
+
+public class Owner {
+
+    private String id;
+    private String resource;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
